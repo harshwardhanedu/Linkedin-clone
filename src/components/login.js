d39d6d7097650e943440b8c6fc62b2805c84edec
harshwardhanedu <harshtitto@gmail.com>
2022-05-10 11:18:19 +0530

    Login-page

--- src/components/login.js
+++ src/components/login.js
@@ -7,34 +7,169 @@ const Login = (props) => {
                 <a href="/">
                     <img src='images/login-logo.svg' />
                 </a>
+                <div>
+                    <Join>Join Now</Join>
+                    <SignIn>Sign In</SignIn>
+                </div>
             </Nav>
+            <Section>
+               <Hero>
+                   <h1>Welcome to your professional community</h1>
+                   <img src='images/login-hero.svg' alt=''/>
+               </Hero>
+               <Form>
+                   <Google>
+                        <img src='images/google.svg' alt='' />
+                        Sign in with Google
+                   </Google>
+               </Form>
+            </Section>
         </Container>
     )
 };
 
 const Container = styled.div`
     padding:5px;
-
 `;
 
 const Nav = styled.nav`
-    max-width:1128px;
-    margin:auto;
-    padding:12px 0 16px;
-    display:flex;
-    align-items:center;
-    position:relative;
-    justify-contain:space-beteween; 
-    flex-wrap:nowrap;
-
+    max-width: 1128px;
+    margin: auto;
+    padding: 12px 0 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-between; 
+    flex-wrap: nowrap;
     & > a{
         width: 135px;
-        heigth: 35px;
+        height: 35px;
         @media (max-width: 786px) {
             padding: 0 5px;
         }
     }
 
+`;
+
+const Join= styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    border-radius: 4px;
+    color: rgba(0, 0, 0, 0.6);
+    margin-right: 12px;
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+        color: rgba(0, 0, 0, 0.9);
+        text-decoration: none;
+
+    }
+`;
+
+const SignIn= styled.a`
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color: #0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    font-height: 40px;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0);
+    &:hover{
+        background-color: rgba(112, 181, 249, 0.15);
+        color: #0a66c2;
+        text-decoration: no;
+    }
+`;
+
+const Section= styled.section`
+display: flex;
+align-content: start;
+min-height: 700px;
+padding-bottom: 128px;
+padding-top: 40px;
+padding: 60px 0;
+position: relative;
+flex-wrap: wrap;
+width: 100%;
+max-width: 1128px;
+align-items: center;
+margin: auto;
+
+@media (max-width: 786px) {
+    margin: auto;
+    min-height: 0px;  
+}
+
+`;
+
+const Hero = styled.div`
+width: 100%;
+h1{
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+        text-align: center;
+        font-size: 20px;
+        width: 100%;
+        line-height: 2;
+        
+    }
+
+    img {
+        z-index: -1; //overlap avoid
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: inherit;
+            height: initial;
+        }
+    }
+}
+`;
+
+const Form = styled.div`
+
+ margin-top: 100px;
+ width: 408px;
+
+ @media (max-width: 756px) {
+     margin-top: 20px;
+     
+ }
+`;
+
+const Google= styled.button`
+display: flex;
+justify-content: center;
+background-color: #fff;
+align-items: center;
+height: 56px;
+width: 100%;
+border-radius: 28px;
+box-shadow: inset 0 0 0 0 1px rgb(0 0 0 / 60%),
+ inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+ vertical-align: middle;
+ z-index: 0;
+ transition-duration: 167ms;
+ font-size: 20px;
+ color: rgba(0, 0, 0, 0.6);
+ &:hover{
+     background-color: rgba(207,207,207,0.25);
+     color: rgba(0,0,0,0.75);
+ }
 `;
 
 export default Login;
