46ad6ad1987a62517390589ea270440aed752e41
harshwardhanedu <harshtitto@gmail.com>
2022-05-10 18:20:06 +0530

    Home-Section1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 import Login from './components/login';
 
 function App() {
   return (
     <div className="wrapper">
       <BrowserRouter>
-        <Login />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
+
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+    return (
+        <Container>
+            <Content>Header</Content>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    background-color: white;
+`;
+
+const Content = styled.div``;
+
+export default Header;
