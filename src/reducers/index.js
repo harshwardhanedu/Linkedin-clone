78c991948ddc930e4ac78a87d0ff96c0e4693728
harshwardhanedu <harshtitto@gmail.com>
2022-05-20 18:51:22 +0530

    changes

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from  "./userReduser" ;
-
-const rootReducer = combineReducers({
-    userState: userReducer,
-});
-
-export default rootReducer;
