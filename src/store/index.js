b17a09d57582d67808e8a35ba984fa1cf3aec15d
harshwardhanedu <harshtitto@gmail.com>
2022-05-22 23:29:34 +0530

    Final

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,7 @@
-// import { legacy_createStore as createStore } from "redux";
-import { legacy_createStore, applyMiddleware } from "redux"; //used legacy_createStore  insted of createStore
+import { applyMiddleware, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
-import rootReducer from "../reducers";
+import rootReducers from "../reducers";
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
 
 export default store;
-
-
