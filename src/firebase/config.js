5366cb40acd6e42e0d656ebe3561d6fd343ab70d
harshwardhanedu <harshtitto@gmail.com>
2022-05-22 23:59:39 +0530

    config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuZl2YJyi5QPjBXFpg1S2kPNc8H8TF6Qk",
+    authDomain: "linkedin-clone-b6f7c.firebaseapp.com",
+    projectId: "linkedin-clone-b6f7c",
+    storageBucket: "linkedin-clone-b6f7c.appspot.com",
+    messagingSenderId: "127146799218",
+    appId: "1:127146799218:web:6a16a29c56216f2948d2eb",
+    measurementId: "G-3CLQZTPV55"
+  }; //test 
+  export default firebaseConfig;
